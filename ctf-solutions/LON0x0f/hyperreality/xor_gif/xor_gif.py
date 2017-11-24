97e41e1d4b710ccb4ffa0e6f5062171b19d999f1
hyperreality <lmt@fastmail.com>
2017-11-24 13:15:51 +0000

    Add LON0x0f crypto images solutions

---
+++ ctf-solutions/LON0x0f/hyperreality/xor_gif/xor_gif.py
@@ -0,0 +1,20 @@
+img = bytearray(open('encrypted_image.gif.xored', 'rb').read())
+
+crib = 'GIF89a'
+
+key_prefix = ''.join([chr(img[i] ^ ord(c)) for i, c in enumerate(crib)])
+print(key_prefix)
+
+with open('websters', 'r') as dic:
+    for word in dic:
+        if word.startswith(key_prefix):
+            key = word
+
+# After a lot of faffing around, key turns out to be 'pestproof' (not 'pestprawn')
+print(key)
+
+xord_byte_array = [img[i] ^ ord(key[i % len(key)]) for i in range(len(img))]
+
+with open('out.gif', 'w') as newFile:
+    newFile.write(bytearray(xord_byte_array))
+
